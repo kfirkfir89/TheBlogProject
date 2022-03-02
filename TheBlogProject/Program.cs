ed75f9def0f05013ab71610a92c1b37a492ead71
kfir <kfirkfir89@gmail.com>
2022-03-03 00:35:26 +0200

    database update
    added postgresql and migrations

--- TheBlogProject/Program.cs
+++ TheBlogProject/Program.cs
@@ -6,9 +6,13 @@ using TheBlogProject.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+//var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+//builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    //options.UseSqlServer(connectionString));
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
+    options.UseNpgsql(connectionString));
+
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 
