5ddc9d6517b0efb4de7fc5c258e8661b4b84de11
kfir <kfirkfir89@gmail.com>
2022-03-13 13:57:07 +0200

    comment controller
    updated the controller and the needed actions and views

--- TheBlogProject/Controllers/CommentsController.cs
+++ TheBlogProject/Controllers/CommentsController.cs
@@ -21,41 +21,35 @@ namespace TheBlogProject.Controllers
         }
 
         // GET: Comments
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> OriginalIndex()
         {
-            var applicationDbContext = _context.Comments.Include(c => c.BlogUser).Include(c => c.Moderator).Include(c => c.Post);
-            return View(await applicationDbContext.ToListAsync());
+            var originalComments = await _context.Comments.ToListAsync();
+            return View("index", originalComments);
         }
+
+        public async Task<IActionResult> ModeratedIndex()
+        {
+            var moderatedComments = await _context.Comments.Where(c => c.Moderator != null).ToListAsync();
+            return View("index", moderatedComments);
+        }
+
+        public async Task<IActionResult> DeletedIndex()
+        {
+            var deletedComments = await _context.Comments.Where(c => c.Deleted != null).ToListAsync();
+            return View("index", deletedComments);
+        }
+
 
         // GET: Comments/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var comment = await _context.Comments
-                .Include(c => c.BlogUser)
-                .Include(c => c.Moderator)
-                .Include(c => c.Post)
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (comment == null)
-            {
-                return NotFound();
-            }
-
-            return View(comment);
-        }
 
         // GET: Comments/Create
-        public IActionResult Create()
-        {
-            ViewData["BlogUserId"] = new SelectList(_context.Users, "Id", "Id");
-            ViewData["ModeratorId"] = new SelectList(_context.Users, "Id", "Id");
-            ViewData["PostId"] = new SelectList(_context.Posts, "Id", "Abstract");
-            return View();
-        }
+        /*        public IActionResult Create()
+                {
+                    ViewData["BlogUserId"] = new SelectList(_context.Users, "Id", "Id");
+                    ViewData["ModeratorId"] = new SelectList(_context.Users, "Id", "Id");
+                    ViewData["PostId"] = new SelectList(_context.Posts, "Id", "Abstract");
+                    return View();
+                }*/
 
         // POST: Comments/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
@@ -70,9 +64,7 @@ namespace TheBlogProject.Controllers
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            ViewData["BlogUserId"] = new SelectList(_context.Users, "Id", "Id", comment.BlogUserId);
-            ViewData["ModeratorId"] = new SelectList(_context.Users, "Id", "Id", comment.ModeratorId);
-            ViewData["PostId"] = new SelectList(_context.Posts, "Id", "Abstract", comment.PostId);
+
             return View(comment);
         }
 
