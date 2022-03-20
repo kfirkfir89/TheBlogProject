edf1ed9c83340ed7baac3aa9fef2372c62d36bd0
kfir <kfirkfir89@gmail.com>
2022-03-20 17:11:26 +0200

    template setup
    start to customize the template and views

--- TheBlogProject/Controllers/HomeController.cs
+++ TheBlogProject/Controllers/HomeController.cs
@@ -18,10 +18,16 @@ namespace TheBlogProject.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult About()
         {
             return View();
         }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
