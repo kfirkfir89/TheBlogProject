4d67371cdebc58adea922d1f5a163a831f9d2378
kfir <kfirkfir89@gmail.com>
2022-03-28 13:49:12 +0300

    email service
    finished with email service and contact me

---
+++ TheBlogProject/ViewModels/ContactMe.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TheBlogProject.ViewModels
+{
+    public class ContactMe
+    {
+        [Required]
+        [StringLength(80, ErrorMessage = "The {0} must be at least {2} and most {1} characters", MinimumLength = 2)]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [StringLength(50, ErrorMessage = "The {0} must be at least {2} and most {1} characters", MinimumLength = 5)]
+        public string Email { get; set; }
+
+        [Phone]
+        public string Phone { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "The {0} must be at least {2} and most {1} characters", MinimumLength = 2)]
+        public string Subject { get; set; }
+
+        [Required]
+        [StringLength(500, ErrorMessage = "The {0} must be at least {2} and most {1} characters", MinimumLength = 2)]
+        public string Message { get; set; }
+    }
+}
