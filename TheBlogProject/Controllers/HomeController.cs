1c8a1352223d83fe478ac0c1a346ae15d8810f55
kfir <kfirkfir89@gmail.com>
2022-05-02 17:42:43 +0300

    setup home index page
    start with pages setup

--- TheBlogProject/Controllers/HomeController.cs
+++ TheBlogProject/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
+using TheBlogProject.Data;
 using TheBlogProject.Models;
 using TheBlogProject.Services;
 using TheBlogProject.ViewModels;
@@ -10,16 +12,36 @@ namespace TheBlogProject.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly IBlogEmailSender _emailSender;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger, IBlogEmailSender emailSender)
+        public HomeController(ILogger<HomeController> logger, IBlogEmailSender emailSender, ApplicationDbContext context)
         {
             _logger = logger;
             _emailSender = emailSender;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var blogs = await _context.Blogs
+                .Include(b => b.BlogUser)
+                .ToListAsync();
+
+            return View(blogs);
+        }
+
+        //BlogPostIndex
+        public async Task<IActionResult> BlogPostIndex(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+            //get the post for specificuler blog
+            var posts = _context.Posts.Where(p => p.Id == id).ToList();
+
+            return View("Index", posts);
         }
 
         public IActionResult About()
