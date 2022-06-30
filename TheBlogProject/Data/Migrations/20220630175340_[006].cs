8f1a7a113b18c32ca2ccf2ef54df23967b2061ca
kfir <kfirkfir89@gmail.com>
2022-06-30 21:05:03 +0300

    added counters
    like useful comments view counters

---
+++ TheBlogProject/Data/Migrations/20220630175340_[006].cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TheBlogProject.Data.Migrations
+{
+    public partial class _006 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Views",
+                table: "Posts",
+                type: "integer",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Views",
+                table: "Posts");
+        }
+    }
+}
