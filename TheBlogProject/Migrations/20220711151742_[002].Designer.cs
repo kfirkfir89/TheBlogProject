4d8551afc727ae2a8bfd5e5d6a59e6eb45a917cc
kfir <kfirkfir89@gmail.com>
2022-07-11 19:10:22 +0300

    fixed tags system db

--- TheBlogProject/Migrations/20220711151742_[002].Designer.cs
+++ TheBlogProject/Migrations/20220711151742_[002].Designer.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using TheBlogProject.Data;
@@ -12,9 +13,10 @@ using TheBlogProject.Data;
 namespace TheBlogProject.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220711151742_[002]")]
+    partial class _002
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
